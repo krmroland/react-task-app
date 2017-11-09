0f89efb3b63808c618dd8f9a75790b6fa8e33555
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2017-11-09 05:48:01 +0300

    Add Task Validation before submission

--- src/AddTask.js
+++ src/AddTask.js
@@ -3,16 +3,40 @@ import React from "react";
 class AddTask extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: "" };
+        this.state = { value: "", error: null };
         this.save = this.save.bind(this);
         this.updateInput = this.updateInput.bind(this);
     }
     save(event) {
         event.preventDefault();
-        this.props.taskAdded(this.state.value);
+        if (this.isValid()) {
+            this.props.taskAdded(this.state.value);
+        }
+    }
+    isValid() {
+        if (!this.state.value) {
+            this.setState({ error: "Please type in something" });
+            return false;
+        }
+        if (this.state.value.length < 3) {
+            this.setState({ error: "Task must atleast be 3 characters" });
+            return false;
+        }
+        if (this.state.value.length > 100) {
+            this.setState({
+                error: "Task cannot  be more than 100 characters"
+            });
+            return false;
+        }
+        return true;
     }
     updateInput(event) {
-        this.setState({ value: event.target.value });
+        //we will check if the given value is valid asynchronously
+        this.setState({ value: event.target.value }, () => {
+            if (this.isValid()) {
+                this.setState({ error: null });
+            }
+        });
     }
     render() {
         return (
@@ -40,11 +64,16 @@ class AddTask extends React.Component {
                                 value={this.state.value}
                                 onChange={this.updateInput}
                             />
+                            {this.state.error &&
+                                <p className="form-text text-danger">
+                                    {this.state.error}
+                                </p>}
                         </div>
                         <div className="form-group">
                             <button
                                 className="btn btn-primary"
                                 onClick={this.save}
+                                disabled={this.state.error}
                             >
                                 Save Task
                             </button>
